16648d4c700edd3fc6204b40abc12bc4e2dacc24
Egoris <egoris@guardiaschool.fr>
2024-12-09 13:02:18 +0100

    add de produit

--- app/fonctionaliter/produit_manager.py
+++ app/fonctionaliter/produit_manager.py
@@ -1,1 +1,13 @@
 # programe de fonctionnaliter du site tout ce qui vat étre de toucher au produit qui existe. 
+from fonctionaliter.produit import Produit
+
+def add_produit():
+    name = input('Nom du produit:')
+    price = float(input( "Prix du produit :"))
+    quantity = int(input("Quantité du produit :"))
+
+    produit = Produit(name,price,quantity)
+    produits = []
+    produits.append(produit)
+    print(f"produit'{name}' et ajouter.")
+
